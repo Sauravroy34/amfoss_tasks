230ff54fccbe2e1a996c14f6d3811613c7b475b6
Sauravroy34 <sauravnotfound29@gmail.com>
2024-02-14 19:20:12 +0530

    done for haskell

--- task_03/Haskell/haskell.hs
+++ task_03/Haskell/haskell.hs
@@ -1,1 +1,14 @@
-main = putStrLn "Hello, Haskell!"
+check :: Int -> Bool
+check 2 = True 
+check n = all(\x -> n `mod` x /=0) [2..n-1]
+
+give :: Int -> [Int]
+give x = filter check [2..x]
+
+main :: IO ()
+main = do 
+    putStrLn "enter a number"
+
+    n <- readLn :: IO Int
+
+    print(give n)
